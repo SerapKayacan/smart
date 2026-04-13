0309258b7ecf3a70706bd12c819f804bf8a61a74
SERAP <kayacan.serap@gmail.com>
2026-04-14 00:12:28 +0300

    Install Log Viewer to debug database server issues

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Models\ServiceCategory;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
+use Opcodes\LogViewer\Facades\LogViewer;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -12,6 +13,10 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        LogViewer::auth(function ($request) {
+            return true;
+        });
+
         View::composer(['frontend.layouts.header', 'frontend.layouts.footer'], function ($view) {
             $view->with('serviceCategories', ServiceCategory::with(['services' => function ($query) {
                 $query->where('is_active', true)->orderBy('sort_order', 'ASC');
